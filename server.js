cc08e34407aaf82978445991456f054e0e7d995d
oharb94 <63370609+oharb94@users.noreply.github.com>
2021-05-10 11:22:41 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+ const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Hello ٍRedis');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
